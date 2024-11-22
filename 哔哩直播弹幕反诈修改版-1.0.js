f278e5d09045b6fbdcb0b40788713277ef5c9e1a
Github1977 <68886053+li7355608@users.noreply.github.com>
2024-11-22 13:30:18 +0800

    Update 哔哩直播弹幕反诈修改版-1.0.js

--- 哔哩直播弹幕反诈修改版-1.0.js
+++ 哔哩直播弹幕反诈修改版-1.0.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         哔哩哔哩直播弹幕反诈修改版
-// @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  发不出去弹幕时不会假装发出去（弹幕列表和播放器上不会出现没发出去的弹幕，且会弹出高亮弹幕提示被什么诈骗），修改自 TGSAN-哔哩哔哩直播弹幕反诈
+// @version      1.1
+// @description  发不出去弹幕时不会假装发出去, 弹幕列表和播放器上不会出现没发出去的弹幕, 且会弹出高亮弹幕提示被什么诈骗
 // @author       cheeys
+// @icon         https://www.bilibili.com/favicon.ico
+// @license      GPL 3.0
 // @match        https://live.bilibili.com/*
 // @run-at       document-start
 // @grant        unsafeWindow
@@ -11,6 +12,32 @@
 
 (function() {
     'use strict';
+
+    //系统过滤器权限高于主播，出现关键词后系统会优先删除你的弹幕，其次才是主播
+
+    //弹幕同屏发送次数，默认为1
+    const exp = 1
+
+    //不同对象屏蔽后显示的弹幕颜色，支持英文和16进制颜色编码
+    const ban_color_system = "#90EE90"
+    const ban_color_user = "deepskyblue"
+
+    //此处调整弹幕滚动速度
+    const speed = 0.07
+
+    // 默认固定从左侧开始滚动的位置
+    const dm_left = '-20%'
+
+    //弹幕距离顶部的位置，如果想要随机可以替换为：`${Math.random() * 100}%`
+    const dm_top = '50%'
+
+    //弹幕字号
+    const dm_fontSize = '36px'
+
+    //弹幕内容
+    const ban_system_msg = "发送失败：你的弹幕被系统秒删，修改关键词后重新发吧"
+    const ban_user_msg = "发送失败：你的弹幕被主播删除，看来主播不喜欢某些关键词"
+
 
     let windowCtx = self.window;
     if (self.unsafeWindow) {
@@ -28,10 +55,10 @@
         const div = document.createElement('div');
         div.textContent = message;
         div.style.position = 'fixed';
-        div.style.top = `${Math.random() * 100}%`;
-        div.style.left = '0%'; // 固定从左侧开始
+        div.style.top = dm_top;
+        div.style.left = dm_left;
         div.style.color = color;
-        div.style.fontSize = '36px';
+        div.style.fontSize = dm_fontSize;
         div.style.zIndex = '9999';
         div.style.whiteSpace = 'nowrap';
         document.body.appendChild(div);
@@ -42,7 +69,7 @@
                 div.remove();
                 return;
             }
-            div.style.left = `${left + 0.07}%`; // 此处调整弹幕速度
+            div.style.left = `${left + speed}%`;
             requestAnimationFrame(animate);
         }
         animate();
@@ -71,18 +98,21 @@
             return new Promise((resolve, reject) => {
                 originFetchBLDMAF(...arg).then(r => {
                     r.json().then(data => {
-                       // console.log(data) 系统过滤器权限高于主播，出现多个诈骗词会优先被系统删除
                         if (data.code === 0 && data.msg === "f") {
-                            showFloatingMessage("发送失败：你的弹幕被系统秒删，你被骗了", "green");
+                            for(let i = 0; i< exp; i++){
+                                showFloatingMessage(ban_system_msg, ban_color_system);
+                            }
                             data.code = -101;
-                            data.message = "没发出去，你被骗了，系统干的";
+                            data.message = "你的弹幕没发出去，你被骗了，系统干的";
                             data.ttl = 1;
                             delete data.msg;
                             delete data.data;
                         } else if (data.code === 0 && data.msg === "k") {
-                            showFloatingMessage("你的弹幕逃过系统秒删但被主播个人过滤器过滤，你又被骗了", "deepskyblue");
+                            for(let i = 0; i< exp; i++){
+                            showFloatingMessage(ban_user_msg, ban_color_user);
+                            }
                             data.code = -101;
-                            data.message = "没发出去，你被骗了，主播干的";
+                            data.message = "你的弹幕没发出去，你被骗了，主播干的";
                             data.ttl = 1;
                             delete data.msg;
                             delete data.data;
